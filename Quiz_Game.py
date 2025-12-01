dfebef15568c3e45681f3336edccc0e3d30b6623
Wiz0903 <162373446+Wiz0903@users.noreply.github.com>
2025-12-01 13:37:30 +0200

    Added options to the game

--- Quiz_Game.py
+++ Quiz_Game.py
@@ -1,20 +1,21 @@
 print("Welcome to the quiz game")
 
 # List of quiz questions and answers stored as dictionaries
-quiz = [{"question": "What is the capital of Free State?", "answer": "Bloemfontein"}, 
-        {"question": "Which national team won the 2022 FIFA world cup?", "answer": "Argentina"}, 
-        {"question": "Which country hosted the 2010 FIFA world cup?", "answer": "South Africa"}, 
-        {"question": "What was the name of the 2010 FIFA world cup ball?", "answer": "Jabulani"}, 
-        {"question": "Which company owns Facebook, Instagram and WhatsApp?", "answer": "Meta"},
-        {"question": "Who is the president of South Africa?", "answer": "Cyril Ramaphosa"},
-        {"question": "Who is the richest person in South Africa?", "answer": "Johann Rupert"},
-        {"question": "Who is the richest black person in South Africa?", "answer": "Patrice Motsepe"}
+quiz = [{"question": "What is the capital of Free State?", "options": ["Bloemfontein", "Cape Town", "Johannesburg"], "answer": "Bloemfontein"}, 
+        {"question": "Which national team won the 2022 FIFA world cup?", "options": ["France", "England", "Argentina"], "answer": "Argentina"}, 
+        {"question": "Which country hosted the 2010 FIFA world cup?", "options": ["France", "Spain", "South Africa"], "answer": "South Africa"}, 
+        {"question": "What was the name of the 2010 FIFA world cup ball?", "options": ["Trionda", "Jabulani", "Brazuca"], "answer": "Jabulani"}, 
+        {"question": "Which company owns Facebook, Instagram and WhatsApp?", "options": ["Meta", "Amazon", "Microsoft"], "answer": "Meta"},
+        {"question": "Who is the president of South Africa?", "options": ["Jacob Zuma", "Cyril Ramaphosa", "Julius Malema"], "answer": "Cyril Ramaphosa"},
+        {"question": "Who is the richest person in South Africa?", "options": ["Patrice Motsepe", "Johann Rupert", "Nicky Oppenheimer"], "answer": "Johann Rupert"},
+        {"question": "Who is the richest black person in South Africa?", "options": ["Cyril Ramaphosa", "Jacob Zuma", "Patrice Motsepe"], "answer": "Patrice Motsepe"}
 ]
 score = 0 # Player's score
 
 # Loop through each question in the quiz list
 for q in quiz:
     print(q["question"]) # Display the current question
+    print(q["options"]) # Display options to choose from
     ans = input() # Get the user's answer
     
     # Check if the user's answer matches the correct one (case-insensitive)
@@ -23,7 +24,7 @@ for q in quiz:
         print("Correct")
     else:
         print("Incorrect")
-        print(f"The correct answer is {q["answer"]}.") # Show the correct answer
+        print(f"The correct answer is {q['answer']}.") # Show the correct answer
         
 score_percent = (score / len(quiz)) * 100 # Show the score percentage
 
